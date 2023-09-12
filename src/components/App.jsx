9c77dc1d9432c0b724509ce34b9f366b0b6f6809
=Andrei <balasca.andrei@gmail.com>
2023-09-12 21:06:47 +0300

    added filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
 import './App.css';
 export const App = () => {
   return (
     <div className="Container">
-      <h1 className="Title">Phonebook📱</h1>
+      <h1 className="Title">Phonebook</h1>
       <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
     </div>
   );
 };
